cd87cfa0769059d2707a96b7ba1baed211cbd693
Tejas Pavan B <160583886+tejaspavanb@users.noreply.github.com>
2025-03-22 12:14:17 +0530

    Update

--- telegram/bot.py
+++ telegram/bot.py
@@ -8,8 +8,7 @@ from datetime import datetime  # Add this import
 from eth_account import Account 
 
 # Initialize Telegram bot
-bot = telebot.TeleBot('INSERT_THE_BOT_API
-')
+bot = telebot.TeleBot('INSERT_THE_BOT_API')
 
 # Connect to Core blockchain
 w3 = Web3(Web3.HTTPProvider('https://rpc.test2.btcs.network'))
